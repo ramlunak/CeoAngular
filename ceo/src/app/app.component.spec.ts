226975d3bcbf9384015e482ffb0878ce62282252
Default Profile <Default@Profile>
2019-06-28 04:32:07 -0300

    plantila en blanco v1

--- ceo/src/app/app.component.spec.ts
+++ ceo/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ceo'`, () => {
+  it(`should have as title 'goldtext'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ceo');
+    expect(app.title).toEqual('goldtext');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ceo!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to goldtext!');
   });
 });
--- ceo/src/app/app.module.ts
+++ ceo/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,   
   ],
   providers: [],
   bootstrap: [AppComponent]
